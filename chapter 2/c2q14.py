369423db8eba5a8193b9cfc3f69bf90ddd156a3b
jonathanmonreal <jem177@case.edu>
2015-07-21 12:43:38 -0400

    add c2q14.py

---
+++ chapter 2/c2q14.py
@@ -0,0 +1,10 @@
+from __future__ import division
+from nltk.corpus import wordnet as wn
+
+def supergloss(s):
+    definitions = 'Definition of ' + str(s) + ': ' + str(s.definition()) + '\n'
+    for hypernym in s.hypernyms():
+        definitions += 'Definition of ' + str(hypernym) + ': ' + str(hypernym.definition()) + '\n'
+    for hyponym in s.hyponyms():
+        definitions += 'Definition of ' + str(hyponym) + ': ' + str(hyponym.definition()) + '\n'
+    return definitions
