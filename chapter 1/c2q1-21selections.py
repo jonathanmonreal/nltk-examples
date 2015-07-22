93b8aebda449573e1219bb4d4ead56c65a115f53
jonathanmonreal <jem177@case.edu>
2015-07-22 12:42:09 -0400

    add c2q1-21selections.py

---
+++ chapter 1/c2q1-21selections.py
@@ -0,0 +1,20 @@
+# Jonathan Monreal
+
+from __future__ import division
+from nltk.book import *
+
+print '1. 12 / (4 + 1) = ' + str(12 / (4 + 1)) + '\n\n'
+
+print '2. 26 ** 100 = ' + str(26 ** 100) + '\n\n'
+
+print '3. [\'Monty\', \'Python\'] * 20 =\n' + str(['Monty', 'Python'] * 20) + '\n'
+sent1 = ['Call', 'me', 'Ishmael', '.']
+print '3 * sent1 =\n' + str(['Call', 'me', 'Ishmael', '.'] * 3) + '\n\n'
+
+print ('4. len([word.lower() for word in text1 if word.isalpha()]) = ' +
+       str(len([word.lower() for word in text1 if word.isalpha()])) + '\n')
+print ('len(set([word.lower() for word in text1 if word.isalpha()])) = ' +
+       str(len(set([word.lower() for word in text1 if word.isalpha()]))) + '\n\n')
+
+print '7. text5.collocations()\n'
+print str(text5.collocations()) + '\n\n'
