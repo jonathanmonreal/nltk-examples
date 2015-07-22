5cb2372a4800e7ef60f2be3c0fae98676a683ea3
jonathanmonreal <jem177@case.edu>
2015-07-22 10:30:12 -0400

    add additional comments to c2q21.py

--- chapter 2/c2q21.py
+++ chapter 2/c2q21.py
@@ -5,21 +5,22 @@ from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 
 def guess_syllables(text):
     
-    prondict = nltk.corpus.cmudict.dict()
+    prondict = nltk.corpus.cmudict.dict() # Stores the pronunciation dictionary
 
-    num_syllables = 0
+    num_syllables = 0                     # Stores the numer of syllables
 
     for word in text:
         average = 0
         try:
+            # There are typically multiple sets of phones for each word
             word_phones = prondict[word]
-            for phones in word_phones:
-                for phone in phones:
+            for phones in word_phones:    # For each set of phone
+                for phone in phones:      # For each individual phone
                     # If there is a digit, it indicates inflection
                     if phone[-1].isdigit():
                         average += 1
-            average /= float(len(word_phones))
-            num_syllables += average
+            average /= float(len(word_phones)) # Average the number of phones
+            num_syllables += average           # Add the average for the word
         except:
             pass
 
