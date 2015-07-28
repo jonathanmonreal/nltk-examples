845dd4d908a7f3376ffd1c00664bce4ccdf15f9b
jonathanmonreal <jem177@case.edu>
2015-07-28 15:54:04 -0400

    update c2q20.py with better implementation

--- chapter 2/c2q20.py
+++ chapter 2/c2q20.py
@@ -3,7 +3,14 @@
 import nltk
 from nltk.corpus import brown
 
+# Implementation using FreqDist (slower, more memory use)
+#def word_freq(word, section):
+#    fdist = nltk.FreqDist(w.lower() for w in brown.words(categories=section))
+#    try:
+#        return fdist[word.lower()]
+
 def word_freq(word, section):
-    fdist = nltk.FreqDist(w.lower() for w in brown.words(categories=section))
     try:
-        return fdist[word.lower()]
+        return brown.words(categories=section).count(word)
+    except:
+        return 0
