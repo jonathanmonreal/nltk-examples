13ab7450b40ca5ca050a46de1b482c7c5f24d0d3
jonathanmonreal <jem177@case.edu>
2015-08-04 12:24:42 -0400

    add q13

--- chapter 3/c3q1-17selections.py
+++ chapter 3/c3q1-17selections.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 import nltk
+from textwrap import fill
 
 s = 'colorless'
 print '1. s = \'colorless\''
@@ -22,16 +23,25 @@ print '3. Yes. You can only wrap around once using negative values. Example: '\
 
 monty = 'monty python'
 print '5. monty[::-1]: ' + monty[::-1]
-print 'The string is printed backwards. This is a reasonable result given that'\
+print fill('The string is printed backwards. This is a reasonable result given that'\
       '[:] specifies the entire string, and using -1 as the step steps through'\
-      'the string backwards.'
+      ' the string backwards.')
 print '\n\n'
 
 print '7.'
 print 'a. Using NLTK: r\'<a|an|the>\'; Otherwise: r\'\s(a|an|the)\s\''
+print '\n\n'
 
 raw = 'this is a test string I am going to split around the character a'
 print '11. raw = \'this is a test string I am going to split around the '\
-      'character a'
+      'character a\''
 print 'raw.split(\'a\'): ' + str(raw.split('a'))
+print '\n\n'
 
+test_string = 'this\t\t \tis a test string'
+print '13. test_string = \'this\t\t \tis a test string\''
+print 'test_string.split(): ' + str(test_string.split())
+print 'test_string.split(\' \'): ' + str(test_string.split(' '))
+print fill('Using .split() ignores all whitespace characters, while using'\
+      '.split(\' \') does not.')
+print '\n\n'
