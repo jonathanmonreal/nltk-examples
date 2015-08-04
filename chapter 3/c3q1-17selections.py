70c6916d3eb41a001ba6ac1b1eb4a5f113ce1c2a
jonathanmonreal <jem177@case.edu>
2015-08-04 11:08:21 -0400

    add q2

--- chapter 3/c3q1-17selections.py
+++ chapter 3/c3q1-17selections.py
@@ -10,3 +10,9 @@ print 's = s[:4] + \'u\' + s[4:]'
 print 's: ' + s
 print '\n\n'
 
+print '2. \'dishes\'[:-2]: ' + 'dishes'[:-2]
+print '\'running\'[:-4]: ' + 'running'[:-4]
+print '\'nationality\'[:-5]: ' + 'nationality'[:-5]
+print '\'undo\'[:-2]: ' + 'undo'[:-2]
+print '\'preheat\'[:-4]: ' + 'preheat'[:-4]
+print '\n\n'
