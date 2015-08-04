2a0d0c486aba6118bc17048b7d3e57e03b03da57
jonathanmonreal <jem177@case.edu>
2015-08-04 11:13:31 -0400

    add q5

--- chapter 3/c3q1-17selections.py
+++ chapter 3/c3q1-17selections.py
@@ -17,7 +17,12 @@ print '\'undo\'[:-2]: ' + 'undo'[:-2]
 print '\'preheat\'[:-4]: ' + 'preheat'[:-4]
 print '\n\n'
 
-print '3. Yes. You can only wrap around once using negative values. Example:'
-print '\'lol\'[-4]'
-'lol'[-4]
-print '\n\n'
+print '3. Yes. You can only wrap around once using negative values. Example: '\
+      '\'lol\'[-4]'
+
+monty = 'monty python'
+print '5. monty[::-1]: ' + monty[::-1]
+print 'The string is printed backwards. This is a reasonable result given that'\
+      '[:] specifies the entire string, and using -1 as the step steps through'\
+      'the string backwards.'
+
