27d92134d7a36a6341f6ca7c0db52055a02f4b25
jonathanmonreal <jem177@case.edu>
2015-08-04 13:45:49 -0400

    add c3q31.py

---
+++ chapter 3/c3q31.py
@@ -0,0 +1,10 @@
+# Jonathan Monreal
+
+saying = ['After', 'all', 'is', 'said', 'and', 'done', ',', 'more', 'is',
+          'said', 'than', 'done', '.']
+lengths = []
+
+for word in saying:
+    lengths.append(len(word))
+
+print lengths
