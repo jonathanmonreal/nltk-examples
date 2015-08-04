62119c84b1e7083bf37fd41f28b7eb1090481571
jonathanmonreal <jem177@case.edu>
2015-08-04 11:07:47 -0400

    add c3q1-17selections.py

---
+++ chapter 3/c3q1-17selections.py
@@ -0,0 +1,12 @@
+# Jonathan Monreal
+
+from __future__ import division
+import nltk
+
+s = 'colorless'
+print '1. s = \'colorless\''
+s = s[:4] + 'u' + s[4:]
+print 's = s[:4] + \'u\' + s[4:]'
+print 's: ' + s
+print '\n\n'
+
