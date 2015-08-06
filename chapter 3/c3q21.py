a8dedfcc652fc34f6c448e483bb8d3ebf2cea8bd
jonathanmonreal <jem177@case.edu>
2015-08-06 14:05:51 -0400

    add c3q21.py

---
+++ chapter 3/c3q21.py
@@ -0,0 +1,19 @@
+# Jonathan Monreal
+
+import re, urllib
+from nltk.corpus import words
+from bs4 import BeautifulSoup as bs
+
+def unknown(url):
+    # get the HTML, as a string
+    html = str(bs(urllib.urlopen(url).read()))
+    # find all substrings
+    substrings = re.findall(r'[a-z]+', html)
+    # specify the wordlist
+    wordlist = words.words()
+    # return the words not in the wordlist
+    return [word for word in substrings if word not in wordlist]
+
+# A lot of the results are HTML tags, javascript, CSS, etc. Also present are
+# words that start with capital letters such that the regular expression cuts
+# off the first letter.
