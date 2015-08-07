0e95b8e7340680ce46599326ca6b8624cd48bd83
jonathanmonreal <jem177@case.edu>
2015-08-06 21:45:00 -0400

    add c3q24.py

---
+++ chapter 3/c3q24.py
@@ -0,0 +1,16 @@
+import re
+
+# dictionary to store the replacements
+replacements = dict([('e', '3'), ('i', '1'), ('l', '|'), ('s', '5'),
+                     ('.', '5w33t!'), ('ate', '8'), ('1', 'ONEONE'),
+                     ('o', '0'), (' s', ' $')])
+
+def hacker(s):
+    # normalize the text
+    s = s.lower()
+    s = re.sub(r'(\ss)|ate|[eiols\.]', replacement, s)
+    print s
+
+# gets the replacement to be used
+def replacement(match):
+    return replacements.get(match.group(0))
