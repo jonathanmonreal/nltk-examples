802f7b324a8a0c0594d791382463aa0e01eb999a
jonathanmonreal <jem177@case.edu>
2015-08-08 09:54:17 -0400

    improve dict declaration in c3q24.py

--- chapter 3/c3q24.py
+++ chapter 3/c3q24.py
@@ -1,9 +1,8 @@
 import re
 
 # dictionary to store the replacements
-replacements = dict([('e', '3'), ('i', '1'), ('l', '|'), ('s', '5'),
-                     ('.', '5w33t!'), ('ate', '8'), ('1', 'ONEONE'),
-                     ('o', '0'), (' s', ' $')])
+replacements = {'e':'3', 'i':'1', 'l':'|', 's':'5', '.':'5w33t!','ate':'8',
+                '1':'ONEONE', 'o':'0', ' s':' $'}
 
 def hacker(s):
     # normalize the text
