6fdf2963bf299cf983255ace4f2b85d60f12ed05
jonathanmonreal <jem177@case.edu>
2015-08-08 13:38:40 -0400

    fix c3q29.py

--- chapter 3/c3q29.py
+++ chapter 3/c3q29.py
@@ -1,11 +1,18 @@
 # Jonathan Monreal
 
+from __future__ import division
 from nltk.corpus import brown
 
-def ari(w, s):
-    return 4.71 * w + 0.5 * s - 21.43
+def ari(words, sentences):
+    chars = 0
+
+    for word in words:
+        chars += len(word)
+    
+    return (4.71 * (chars / len(words)) + 0.5 * (len(words) / len(sentences))
+            - 21.43)
 
 for category in brown.categories():
-    print '%*s %9d' % (max(len(c) for c in brown.categories()), category,
-                             ari(len(brown.words(categories = category)),
-                                     len(brown.sents(categories = category))))
+    print '%*s %9f' % (max(len(c) for c in brown.categories()), category,
+                             ari(brown.words(categories = category),
+                                 brown.sents(categories = category)))
