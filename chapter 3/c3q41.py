fb6f28600be94f47ea719fd71517202b3dec9046
jonathanmonreal <jem177@case.edu>
2015-08-10 02:43:09 -0400

    fix line length in c3q41.py

--- chapter 3/c3q41.py
+++ chapter 3/c3q41.py
@@ -3,5 +3,6 @@
 words = ['attribution', 'confabulation', 'elocution', 'sequoia', 'tenacious',
          'unidirectional']
 
-vsequences = set([''.join([char for char in word if char in 'aeiou']) for word in words])
+vsequences = set([''.join([char for char in word if char in 'aeiou']) for word
+                  in words])
 print sorted(vsequences)
